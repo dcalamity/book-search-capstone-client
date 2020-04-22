9f3ab9a10e18f8b92abccd74a2f98de0b97f4ad3
Eduardo Gonzalez <eg9800@gmail.com>
2020-04-22 13:42:59 -0700

    added more changed to book info side of app

--- src/Bookinfo.js
+++ src/Bookinfo.js
@@ -8,53 +8,49 @@ function Bookinfo() {
       <h1>Book Info</h1>
       <header className="d-header">
           <div className="top-left">Logo</div>
-          {/* <div className="h-center">
-              <label>Book Title</label>
-              <input type="text" name='Title' placeholder="Book Title"/>
-              <button type="submit" placeholder="search">
-                <a href="dashboardBooklist.html">Search</a>
-              </button>
-          </div> */}
           <div className="top-right">
             <ul className='link'>
               <li><a href="/user/dash">Home</a></li>
               <li><a href="/">Log Out</a></li>
             </ul>
-            
           </div>
-        </header>
+     </header>
 
       <main className="b-square">
 
         <div className="book-cover">
-          <h2>Book Title</h2>
+          <h2>Book</h2>
         </div>
         
         <div className="mero-mole">
           <div>
-            <h3>Title</h3>
-            <h3>Author</h3>
-            <p>Comments</p>
+            <h3>Title: The perks of being a wallflower</h3>
+            <h3>Author: T.R. Reid</h3>
+            <h4>Description: </h4>
+            <h2>Comments</h2>
+            <div className="bubble-list">
+                <div className="bubble clearfix">
+                  <div className="bubble-content">
+                    <p>Here is a comment I made</p>
+                  </div>
+                </div>
+            </div>
+            <div className="bubble-list">
+                <div className="bubble clearfix">
+                  <div className="bubble-content">
+                    <p>Here is a comment I made</p>
+                  </div>
+                </div>
+            </div>
           </div>
           <div className="infomiddle">
             <form action="#" method="post" className='textbox'>
-            <textarea> 
-              This is a simple comment box. 
+            <textarea className='commentbox' placeholder="Add comment.." > 
+              
             </textarea>
-            <button type='submit'>Submit</button>
+            <button type='submit' className="sbtcm" >Submit</button>
           </form>
           </div> 
-          <div>
-            <label>Choose a collection:</label>
-            <select id="Collections" label="collection names">
-              <option value="Scary Books">
-                Scary Books
-              </option>
-              <option value="Funny Books">
-                Funny Books
-              </option>
-            </select> 
-          </div>  
         </div>
 
       </main>
